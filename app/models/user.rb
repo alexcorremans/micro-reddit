3e83f46028014c4bc874ff1ed69e8f92d0e4dd31
Alexandra Corremans <alexandra.corremans@gmail.com>
2019-01-20 17:35:42 +1100

    add User model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :username,  presence: true,
+                        length: { in: 4..12 },
+                        uniqueness: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password,  presence: true,
+                        length: { in: 6..16 }
+end
